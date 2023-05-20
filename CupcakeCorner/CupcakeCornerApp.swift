5cf9dc3f9f0ea2c7254b3f4c9c4033ee393fd3ca
SeikaHirori <SeikaHirori@outlook.com>
2023-05-20 16:19:51 -0700

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Seika Hirori on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
