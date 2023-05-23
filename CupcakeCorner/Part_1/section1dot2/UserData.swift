31facf4e42fb7116bcc21c951eaa6fa0ab0e6248
SeikaHirori <SeikaHirori@outlook.com>
2023-05-23 14:28:28 -0700

    Moved View and User into folder "section1dot2"

---
+++ CupcakeCorner/Part_1/section1dot2/UserData.swift
@@ -0,0 +1,13 @@
+//
+//  UserData.swift
+//  CupcakeCorner
+//
+//  Created by Seika Hirori on 5/23/23.
+//
+
+import Foundation
+
+
+class UserData: ObservableObject, Codable {
+    var name: String = "Paul Hudson"
+}
---
+++ CupcakeCorner/Part_1/section1dot2/ViewSection1dot2.swift
@@ -0,0 +1,24 @@
+//
+//  ViewSection1dot2.swift
+//  CupcakeCorner
+//
+//  Created by Seika Hirori on 5/23/23.
+//
+
+import SwiftUI
+
+struct ViewSection1dot2: View {
+    private var userData: UserData = UserData()
+    
+    var body: some View {
+        return VStack {
+            Text("Hello, \(userData.name)")
+        }
+    }
+}
+
+struct ViewSection1dot2_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewSection1dot2()
+    }
+}
