27b7d47487f358f5b2faa52c14feb7b0f7e1706f
SeikaHirori <SeikaHirori@outlook.com>
2023-05-23 15:19:13 -0700

    Added encode to conform to Codable

--- CupcakeCorner/Part_1/section1dot2/UserData.swift
+++ CupcakeCorner/Part_1/section1dot2/UserData.swift
@@ -19,4 +19,9 @@ class UserData: ObservableObject, Codable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         name = try container.decode(String.self, forKey: .name)
     }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
 }
